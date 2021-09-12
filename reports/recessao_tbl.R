9f5c11304af8bcd95016b51b39b6a4b9d0f2ce3e
gabriel-lr <glopesrodrigues94@gmail.com>
2021-09-11 21:18:58 -0300

    att

--- reports/recessao_tbl.R
+++ reports/recessao_tbl.R
@@ -1,7 +1,7 @@
 # Harding Pagan vs CODACE
 
 # Coleta e tratamento de dados
-pib <- sidrar::get_sidra(api = "/t/1621/n1/all/v/all/p/all/c11255/90707/d/v584%202") |> 
+pib <- sidrar::get_sidra(api = "/t/1621/n1/all/v/all/p/all/c11255/90707/d/v584%202") 
   
 pib <- pib |> 
   dplyr::select("date" = `Trimestre (Código)`, "value" = `Valor`)  |> 
