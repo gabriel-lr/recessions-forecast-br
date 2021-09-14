931cfe87e03e49cc707a49e262b359b1cd34cd11
gabriel-lr <glopesrodrigues94@gmail.com>
2021-09-13 22:22:57 -0300

    att apr

--- eda/eda.R
+++ eda/eda.R
@@ -2,56 +2,97 @@
 
 ## Plotando
 
-df.plot <- df |> 
-  full_join(index_tcb, by = "data") |> 
-  inner_join(codace, by = "data") |> 
-  drop_na()
-
-# Função Plot
-attach(df.plot)
-
-rec_plot <- function(y, titulo, cor){
-  df.plot |> 
-    ggplot(aes(x = data, y = y))+
-    geom_line(colour = cor, size = 0.7)+
-    geom_ribbon(
-      aes(x = data,
-          ymax = ifelse(dummy == 0, max(y),min(y)), 
-          ymin = min(y), 
-          alpha=0.5))+
-    ylim(min(y),max(y))+
-    ggtitle(titulo) +
-    xlab("Ano") + ylab("Index")+
-    theme(
-      plot.title = element_text(color="black", size=10, face="bold"),
-      axis.title.x = element_text(color="gray", size=10, face="bold"),
-      axis.title.y = element_text(color="gray", size=10, face="bold"),
-      legend.position = "none"
-      )
-    
-}
-
+df.plot <- my_data
 
 ## Indice Deterioração Macro
-ind_det <- rec_plot(y = tcb, titulo = "Índice Deterioração Macro", cor ="blue")
-ggplotly(ind_det)
+
+tcb.plot <- df.plot |>
+  dplyr::select(data, tcb, dummy) |> 
+  tidyr::drop_na()
+
+tcb.plot$tcb <- log(tcb.plot$tcb)
+p <- ggplot(tcb.plot,
+       aes(x = data, y = tcb))+
+  geom_line(size = 0.7)+
+  geom_ribbon(aes(x = data,
+                  ymax = ifelse(dummy == 1, max(tcb),min(tcb)), 
+                  ymin = min(tcb), 
+                  alpha=0.5))+
+  ggtitle("Índice de Deterioração Macroeconômica")+
+  ggplot2::xlab("Ano") + ggplot2::ylab("Index")
+  
+plotly::ggplotly(p)
 
 ## Indice de Liquidez
+il.plot <- df.plot |>
+  dplyr::select(data, il, dummy) |> 
+  tidyr::drop_na()
 
-il_plot <- rec_plot(y = il, titulo = "Indice de Liquidez Bacen", cor = "orange")
-ggplotly(il_plot)
+il.plot$il <- log(il.plot$il)
+p <- ggplot(il.plot,
+            aes(x = data, y = il))+
+  geom_line(size = 0.7)+
+  geom_ribbon(aes(x = data,
+                  ymax = ifelse(dummy == 1, max(il),min(il)), 
+                  ymin = min(il), 
+                  alpha=0.5))+
+  ggtitle("Índice de Liquidez")+
+  ggplot2::xlab("Ano") + ggplot2::ylab("Index")
 
-## FIPEZAP
-fipe_plot <- rec_plot(y = fipezap, cor = "red", titulo = "FIPEZAP")
-ggplotly(fipe_plot)
+plotly::ggplotly(p)
+
+
+ ## FIPEZAP
+
+fp.plot <- df.plot |>
+  dplyr::select(data, fipezap, dummy) |> 
+  tidyr::drop_na()
+
+
+p <- ggplot(fp.plot,
+            aes(x = data, y = log(fp)))+
+  geom_line(size = 0.7)+
+  geom_ribbon(aes(x = data,
+                  ymax = ifelse(dummy == 1, max(log(fp)),min(log(fp))), 
+                  ymin = min(log(fp)), 
+                  alpha=0.5))+
+  ggtitle("Índice Fipezap")+
+  ggplot2::xlab("Ano") + ggplot2::ylab("Index")
+
+plotly::ggplotly(p)
 
 ## Ibovespa
 
-ibov_plot <- rec_plot(y = ibov, cor = "green", titulo = "IBOVESPA")
-ggplotly(ibov_plot)
+ibov.plot <- df.plot |>
+  dplyr::select(data, ibov, dummy) |> 
+  tidyr::drop_na()
+
+p <- ggplot(ibov.plot,
+            aes(x = data, y = ibov))+
+  geom_line(size = 0.7)+
+  geom_ribbon(aes(x = data,
+                  ymax = ifelse(dummy == 1, max(ibov),min(ibov)), 
+                  ymin = min(ibov), 
+                  alpha=0.5))+
+  ggtitle("Índice IBOVESPA")+
+  ggplot2::xlab("Ano") + ggplot2::ylab("Index")
+
+plotly::ggplotly(p)
 
 ## Swap
 
-swap_plot <- rec_plot(y = swap, cor = "skyblue", titulo = "Swap DI 360")
+swap.plot <- df.plot |>
+  dplyr::select(data, swap, dummy) |> 
+  tidyr::drop_na()
 
-ggplotly(swap_plot)
+p <- ggplot(swap.plot,
+            aes(x = data, y = log(swap)))+
+  geom_line(size = 0.7)+
+  geom_ribbon(aes(x = data,
+                  ymax = ifelse(dummy == 1, max(log(swap)),min(log(swap))), 
+                  ymin = min(log(swap)), 
+                  alpha=0.5))+
+  ggtitle("Tx. Swap DI/Pré Fixada 360 dias")+
+  ggplot2::xlab("Ano") + ggplot2::ylab("Index")
+
+plotly::ggplotly(p)
